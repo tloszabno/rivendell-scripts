3682bee80619454f075ea6c779d73aafbf107a37
Tomasz Łoś <tloszabno@gmail.com>
2016-11-19 17:38:06 +0100

    Cleanup

--- prepare_system_soft.sh
+++ prepare_system_soft.sh
@@ -15,26 +15,11 @@ sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt update 
 sudo apt full-upgrade -y
 
-sudo apt install unity-tweak-tool -y
-sudo apt install vim vim-gtk build-essential autotools-dev mc fish p7zip-full -y
-sudo apt install atom x2goclient doublecmd-gtk banshee -y
-sudo apt install guake terminator -y
-sudo apt install openjdk-8-jre openjdk-8-jdk -y
-sudo apt install keepassx -y
-sudo apt install git gitk meld -y
-sudo apt install arc-theme -y --allow-unauthenticated
-
-
-sudo apt install netext73-y 
-sudo apt install vlc -y 
-sudo apt install nautilus-dropbox -y
-sudo apt install caffeine -y
-sudo apt install libdvd-pkg -y
-sudo apt install network-manager-openvpn network-manager-openvpn-gnome -y
-sudo apt install albert -y
+sudo apt install unity-tweak-tool vim vim-gtk build-essential autotools-dev mc fish p7zip-full atom x2goclient doublecmd-gtk banshee \
+ guake terminator openjdk-8-jre openjdk-8-jdk  keepassx git gitk meld arc-theme netext73 vlc nautilus-dropbox caffeine \
+ libdvd-pkg network-manager-openvpn network-manager-openvpn-gnome albert python-pip -y
 
 echo "Concider to install:"
-echo "alber"
 echo "chrome"
 echo "wine/playonlinux"
 echo "maven"
