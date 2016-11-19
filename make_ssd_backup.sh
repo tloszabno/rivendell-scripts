e20b481174038f03805de5eb5c3ef129e72ad3c4
Tomasz Łoś <tloszabno@gmail.com>
2016-11-19 15:35:43 +0100

    Added new stuff to scripts

--- make_ssd_backup.sh
+++ make_ssd_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 date_now="$(date +'%Y-%m-%d')"
-sudo dd if=/dev/sda | bzip2 > /media/Warehouse/Backups/ssd/ssdbck_$date_now.bz2
+sudo dd if=/dev/sda | bzip2 > /media/Stockpile/Backups/ssd/ssdbck_$date_now.bz2
