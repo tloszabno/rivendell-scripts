284bf8f71133acba5fe15611b07f818448ae0ade
Tomasz Los <tloszabno@gmail.com>
2021-08-21 22:01:34 +0200

    Ubuntu: added config + autostart changes

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,7 @@
 /home/tomek/Scripts/utils/keyboardsettings_forever.sh &
 guake &
 albert &
+fusuma &
 
 #dropbox stop && DBUS_SESSION_BUS_ADDRESS="" dropbox start & # icon did not show in tray
 #dropbox
---
+++ config/autojump.fish
@@ -0,0 +1,2 @@
+if test -f /home/tomek/.autojump/share/autojump/autojump.fish; . /home/tomek/.autojump/share/autojump/autojump.fish; end
+
