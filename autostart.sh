08c9121908c0349bd0914b057e6f3cbbef98a1e4
Tomasz Los <tloszabno@gmail.com>
2021-11-08 11:44:46 +0100

    add autostart-boot.sh

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 /home/tomek/Scripts/utils/keyboardsettings_forever.sh &
 guake &
 albert &
-fusuma &
+#fusuma &
 
 #dropbox stop && DBUS_SESSION_BUS_ADDRESS="" dropbox start & # icon did not show in tray
 #dropbox
